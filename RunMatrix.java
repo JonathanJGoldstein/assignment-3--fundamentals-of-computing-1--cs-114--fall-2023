8b6cb714ff93d4259b36d1e4f5213c15c3eae7ca
jgoldstei@hartford.edu <jgoldstei@hartford.edu>
2023-10-30 18:39:14 -0400

    Add calls to matrix object
    -Added code to allow the user to give a size array. have it populated by an incremented value, then inverted along the y=x axis.

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    int user_input = 0;
+    Scanner myscanner = new Scanner(System.in);
+    Matrix myMatrix = new Matrix();
+    user_input = myscanner.nextInt();
+    myMatrix.initializeMatrix(user_input);
+    myMatrix.printMatrix();
+    myMatrix.populateMatrix();
+    myMatrix.printMatrix();
+    myMatrix.invertMatrixDiagonal();
+    myMatrix.printMatrix();
+
+    myscanner.close();
   }
 }
